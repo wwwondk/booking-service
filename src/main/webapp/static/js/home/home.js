c571e0827250e7e390177a8a3cba17a1270771fd
smilesj <19smilesj@gmail.com>
2018-06-22 15:58:55 +0900

    [FE] 상품리스트 페이징
    - 스크롤 기능 추가

--- src/main/webapp/static/js/home/home.js
+++ src/main/webapp/static/js/home/home.js
@@ -1,11 +1,16 @@
 
 $(function(){
-	console.log('load home.js');
 
 	// 카테고리
 	var categoryId = 0;
 	var page = 0;
+	var maxPage = 1;
 	
+	// 상품리스트
+	var source = $('#product-template').html();
+	var templete = Handlebars.compile(source);
+	
+	// 이벤트 바인딩
 	$('ul.event_tab_lst').on('click', 'li.item', clickCategory);
 	
 	function clickCategory(e){
@@ -16,26 +21,26 @@ $(function(){
 		$('ul.event_tab_lst li.item a.active').removeClass('active');
 		$(e.currentTarget).find('a').addClass('active');
 		$('#product_count').text(categoryCount+'개');
+		
+		maxPage = (categoryCount%10 === 0) ? categoryCount%10 : categoryCount%10+1;
+		
+		// 새 카레고리에 관련 초기화
+		page = 0;
+		$("#left_event_box").empty();
+		$("#right_event_box").empty();
 		getProductList();
 	}
 	
 	// 상품리스트 가져오기
 	function getProductList(){
-		page = 0;
 		$.ajax({
 			url : '/categories/'+categoryId+'/products',
 			method : 'GET',
 			data : {'page' : page}
 		}).done(function(res){
 
-			var source = $('#product-template').html();
-			var templete = Handlebars.compile(source);
-			
 			var leftItemList = [];
 			var rightItemList = [];
-			
-			$("#left_event_box").empty();
-			$("#right_event_box").empty();
 			
 			for (var i = 0, l = res.length; i < l; i++) {
 	            if (i % 2) {
@@ -50,5 +55,16 @@ $(function(){
 		});
 	}
 	
-})
+	// 스크롤
+	$(window).scroll(function(){
+		var height = $(document).scrollTop();
+		if($(window).scrollTop() === $(document).height() - $(window).height()){
+			if(page < maxPage){
+				page++;
+				getProductList();
+			}
+		}
+	});
+	
+});
 
