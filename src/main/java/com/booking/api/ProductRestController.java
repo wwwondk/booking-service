0453366da175db6dbd552ddcb7042dfb1077ddec
smilesj <19smilesj@gmail.com>
2018-06-22 15:25:14 +0900

    [BE] 메인페이지 상품리스트 가져오기

---
+++ src/main/java/com/booking/api/ProductRestController.java
@@ -0,0 +1,31 @@
+package com.booking.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.booking.dto.ProductDto;
+import com.booking.service.ProductService;
+import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
+
+@RestController
+@RequestMapping("/categories")
+public class ProductRestController {
+
+	private ProductService productService;
+	
+	@Autowired
+	public ProductRestController(ProductService productService) {
+		this.productService = productService;
+	}
+	
+	@GetMapping("/{id}/products")
+	public List<ProductDto> getList(@PathVariable("id") int categoryId, @RequestParam int page) {
+		return productService.getList(categoryId, page);
+	}
+}
