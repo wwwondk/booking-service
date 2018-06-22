f161a211db5215511ae25b722742dba05996c32c
smilesj <19smilesj@gmail.com>
2018-06-22 15:25:41 +0900

    [FE] 메인페이지 상품 리스트 가져오기
    - HandleBar

--- src/main/webapp/static/js/home/home.js
+++ src/main/webapp/static/js/home/home.js
@@ -4,6 +4,8 @@ $(function(){
 
 	// 카테고리
 	var categoryId = 0;
+	var page = 0;
+	
 	$('ul.event_tab_lst').on('click', 'li.item', clickCategory);
 	
 	function clickCategory(e){
@@ -14,6 +16,39 @@ $(function(){
 		$('ul.event_tab_lst li.item a.active').removeClass('active');
 		$(e.currentTarget).find('a').addClass('active');
 		$('#product_count').text(categoryCount+'개');
+		getProductList();
 	}
+	
+	// 상품리스트 가져오기
+	function getProductList(){
+		page = 0;
+		$.ajax({
+			url : '/categories/'+categoryId+'/products',
+			method : 'GET',
+			data : {'page' : page}
+		}).done(function(res){
+
+			var source = $('#product-template').html();
+			var templete = Handlebars.compile(source);
+			
+			var leftItemList = [];
+			var rightItemList = [];
+			
+			$("#left_event_box").empty();
+			$("#right_event_box").empty();
+			
+			for (var i = 0, l = res.length; i < l; i++) {
+	            if (i % 2) {
+	            	rightItemList.push(res[i]);
+	            } else {
+	            	leftItemList.push(res[i]);
+	            }
+	        }
+
+			$("#left_event_box").append(templete({products: leftItemList}));
+			$("#right_event_box").append(templete({products: rightItemList}));
+		});
+	}
+	
 })
 
