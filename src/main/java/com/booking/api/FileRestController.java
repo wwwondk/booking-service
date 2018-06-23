b9f75a26b1267028f9b866fe35fdc5e487a8af44
smilesj <19smilesj@gmail.com>
2018-06-23 23:40:55 +0900

    [BE] file REST API

---
+++ src/main/java/com/booking/api/FileRestController.java
@@ -0,0 +1,34 @@
+package com.booking.api;
+
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.booking.service.FileService;
+
+@RestController
+@RequestMapping("/files")
+public class FileRestController {
+	
+    private FileService fileService;
+
+    @Autowired
+    public FileRestController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @GetMapping("/{id:[\\d]+}")
+    public FileSystemResource test(@PathVariable int id) {
+        File file = new File(fileService.selectSaveFileName(id));
+        if (!file.exists()) {
+            throw new RuntimeException("File Not Found");
+        } else {
+            return new FileSystemResource(file);
+        }
+    }
+}
