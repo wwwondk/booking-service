8eecf4a4aa4db0da960c1e833d1cb02ac1f7b87f
smilesj <19smilesj@gmail.com>
2018-07-04 11:46:52 +0900

    [FE] 별점

--- src/main/webapp/static/js/reviewWrite/reviewWrite.js
+++ src/main/webapp/static/js/reviewWrite/reviewWrite.js
@@ -1,1 +1,31 @@
-console.log('reviewWrite LOAD')
+$(function(){
+	
+	// 별점
+	var starPoint = 0;
+	var starIndex = 0;
+	var preIndex = 0;
+	$('.rating .rating_rdo').on('click', function(e){
+		preIndex = starIndex;
+		starIndex = $(e.currentTarget).val();
+		
+		var isChecked = $(e.currentTarget).prop('checked');
+
+		if(starIndex === '1' && isChecked === false && preIndex === starIndex){
+			$('.rating_rdo').prop('checked', false);
+			starIndex--;
+			$('.star_rank ').text(starIndex);
+			return;
+		}
+		
+		for(var i = 1; i <= 5; i++){	
+			if(i <= starIndex){
+				$('.rating_rdo[name=rating'+i+']').prop('checked', true);
+			}else{
+				$('.rating_rdo[name=rating'+i+']').prop('checked', false);
+			}
+		}
+		
+		$('.star_rank ').text(starIndex);
+	
+	});
+});
