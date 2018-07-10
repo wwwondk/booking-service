e98155323401c64ea12271f44ee4474180f097dd
smilesj <19smilesj@gmail.com>
2018-07-10 17:57:29 +0900

    [FE] 예매 중..

--- src/main/webapp/static/js/reserve/reserve.js
+++ src/main/webapp/static/js/reserve/reserve.js
@@ -3,5 +3,39 @@ console.log('RESERVE JS');
 $(function(){
 	
 	$('.header').attr('display', 'none');
+	
+	//var ticket = $('.qty');
+	//console.log(ticket);
+	function Ticket(){
+		var count;
+		function init(){
+			this.count = 0;
+			bindEvents();
+			console.log(this);
+		}
+		function bindEvents(){
+			$('.ico_minus3').on('click', minus);
+		}
+		function plus(){
+			console.log('plus!');
+			return ++count;
+		}
+		function minus(){
+			console.log('minus!');
+			return --count;
+		}
+		return {
+			init : init
+		}
+	}
+	
+	
+	var tickets = [];
+    $(".qty").each(function (i, e) {
+    	var v = new Ticket('#' + $(e).attr('id'));
+        tickets.push(v);
+        v.init();
+    });
+	
 });
 
