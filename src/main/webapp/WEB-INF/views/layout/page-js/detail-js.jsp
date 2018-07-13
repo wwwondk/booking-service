09133c3ced5548876103b608e36b89f48bfb3832
smilesj <19smilesj@gmail.com>
2018-07-13 23:14:34 +0900

    [FE] '디테일' 페이지 네이버지도 모듈 분리

--- src/main/webapp/WEB-INF/views/layout/page-js/detail-js.jsp
+++ src/main/webapp/WEB-INF/views/layout/page-js/detail-js.jsp
@@ -1,4 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <script type="text/javascript" src="https://openapi.map.naver.com/openapi/v3/maps.js?clientId=CA09P0ovGU8wMELmLUNF&submodules=geocoder"></script>
+<script type="text/javascript" src="/static/js/detail/naverMap.js"></script>
 <script type="text/javascript" src="/static/js/detail/detail.js"></script>
--- src/main/webapp/static/js/detail/detail.js
+++ src/main/webapp/static/js/detail/detail.js
@@ -81,46 +81,6 @@ $(function(){
 		}
 	});
 	
-	var NaverMap = (function () {
-	    var map = new naver.maps.Map('map');
-	    var myAddress;
-	    var pointX;
-	    var pointY;
-
-	    function init() {
-	    	myAddress = $('.store_addr.store_addr_bold').text();
-	        getPosition();
-	    }
-
-	    function getPosition() {
-	        naver.maps.Service.geocode({address: myAddress}, setGeocode);
-	    }
-
-	    function setGeocode(status, response) {
-	        if (status !== naver.maps.Service.Status.OK) {
-	        	console.log(myAddress + '의 검색 결과가 없습니다.');
-	            return;
-	        }
-	        var result = response.result;
-	        var myaddr = new naver.maps.Point(result.items[0].point.x, result.items[0].point.y);
-	        pointX = result.items[0].point.x;
-	        pointY = result.items[0].point.y;
-	        drawMap();
-	    }
-
-	    function drawMap() {
-	        var mapSrc = 'https://openapi.naver.com/v1/map/staticmap.bin?clientId=CA09P0ovGU8wMELmLUNF&url=http://localhost&crs=EPSG:4326&center=' + pointX + ',' + pointY + '&level=11&w=300&h=250&baselayer=default&markers=' + pointX + ',' + pointY;
-	        $('.store_map').attr('src', mapSrc);
-
-	        var mapUrl = "http://map.naver.com/?lng=" + pointX + "&pinTitle=" + $('.addr_detail').text() + "&level=2&pinType=SITE&lat=" + pointY + "&enc=utf8";
-	        $('.store_location').attr('href', mapUrl);
-	    }
-
-	    return {
-	        init: init
-	    }
-	})();
-	
 	NaverMap.init();
 	
 });
